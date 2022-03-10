03af062322e7eb10ffc928da7b23d3ec79b3e60d
Ho kai Man <hman@f0r3s5.codam.nl>
2022-03-10 14:09:03 +0100

    added envp_copy file

---
+++ envp_copy/builtin.h
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../lib/libft/libft.h"
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*prev;
+	struct s_env	*next;
+}				t_env;
+
+int	ft_strcmp(const char *s1, const char *s2);
+
+//function of envp_list_utils
+void	insert_back_of_list(t_env **begin_list, t_env *node);
+void	insert_node_sorted(t_env **begin_list, t_env *node);
+void	set_env(t_env **envp_list, char *key, char *value);
+void	del_env(t_env **envp_list, char *key);
+char	*get_env(t_env *envp_list, char *key);
+
+//functions in  envp_copy
+t_env	*create_node(char *key, char *value);
+char	**ft_split_env(char *env);
+void	free_envp_list(t_env *list);
+t_env	*env_copy(char **envp);
